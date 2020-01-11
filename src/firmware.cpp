447c25a0862b142da3a830955cb2fda63cb7c761
John Wass <wassj@ctc.com>
2020-01-10 21:53:31 -0500

    sketch in oled

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -1,4 +1,16 @@
+#include <Particle.h>
+#include <Adafruit_SSD1306.h>
+
+
+Adafruit_SSD1306 tft(128, 32);
 void setup() {
+   tft.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+   tft.setTextColor(WHITE);
+   tft.setTextSize(1);
+
+   tft.print("ready");
+   tft.display();
 }
 
 void loop() {
