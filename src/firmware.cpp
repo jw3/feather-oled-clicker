ed85c920226eba88c273889f7b0c9695ab131560
John Wass <wassj@ctc.com>
2020-01-15 15:01:17 -0500

    clicking selections

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -4,13 +4,14 @@
 
 SYSTEM_MODE(MANUAL)
 
-const uint16_t DataPin = D2;
-const uint16_t ClockPin = D3;
-const uint16_t SwitchPin = D6;
+const uint16_t DataPin = D6;
+const uint16_t ClockPin = D8;
+const uint16_t SwitchPin = D7;
 
 int32_t vclkPrev = 0;
 int32_t encoderIdx = 0;
 
+SerialLogHandler logHandler;
 auto clicklist = std::vector<std::string>{};
 
 Adafruit_SSD1306 tft(128, 32);
@@ -24,11 +25,12 @@ void setup() {
    tft.setTextColor(WHITE);
    tft.setTextSize(1);
    tft.setCursor(0, 0);
-   //tft.print("hi john boy, this is a computer program");
+   tft.print("initializing");
    tft.display();
 
    pinMode(DataPin, INPUT);
    pinMode(ClockPin, INPUT);
+   pinMode(SwitchPin, INPUT);
 
    vclkPrev = digitalRead(ClockPin);
 
@@ -62,6 +64,16 @@ void loop() {
       }
    }
 
+   if(digitalRead(SwitchPin) == LOW) {
+      Log.info("CLICKED!");
+      tft.print("!");
+      tft.display();
+
+      auto idx = encoderIdx % clicklist.size();
+      if(idx < 0) idx += clicklist.size();
+      Serial.printlnf("X=%i", idx);
+   }
+
    const auto vclk = digitalRead(ClockPin);
    if(vclk != vclkPrev) {
       if(digitalRead(DataPin) != vclk)
@@ -72,8 +84,10 @@ void loop() {
       if(idx < 0) idx += clicklist.size();
 
       tft.clearDisplay();
+      tft.setCursor(0, 0);
       tft.print(clicklist.at(idx).c_str());
-      tft.printf("%i", encoderIdx);
+      tft.printf(" %i", encoderIdx);
+      Log.info("turned to index: %li", encoderIdx);
       tft.display();
    }
    vclkPrev = vclk;
