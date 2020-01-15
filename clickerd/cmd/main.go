c7cfcda33dfa75ee7bd642ba67d46816270028d6
John Wass <wassj@ctc.com>
2020-01-15 14:58:48 -0500

    listen for selections and run command

--- clickerd/cmd/main.go
+++ clickerd/cmd/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/go-yaml/yaml"
 	"github.com/tarm/serial"
 	"io/ioutil"
 	"log"
+	"os/exec"
+	"strconv"
 	"strings"
 )
 
 type Cfg struct {
-	Uri   string
-	Items [] struct {
+	Command     string
+	Items       [] Item
+	Concurrency int
+}
+
+type Item struct {
+	Id      string
+	Title   string
+	Modules [] struct {
 		Id    string
-		Title string
-		Uri   string
-		Body  string
+		Model string
 	}
 }
 
 func main() {
 	log.Println("starting clickerd")
 
-	// connect to serial
+	// connect to serial; should read from arg
 	c := &serial.Config{Name: "/dev/ttyACM0", Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
@@ -66,6 +74,18 @@ func main() {
 		}
 	}()
 
+	// read def file
+	cfg := Cfg{}
+	cfgf, e := ioutil.ReadFile(".local/clickerd.conf")
+	if e != nil {
+		log.Fatalf("failed to read configuration: %v", e)
+	}
+
+	if e := yaml.UnmarshalStrict([]byte(cfgf), &cfg); e != nil {
+		log.Fatalf("failed to unmarshal configuration: %v", e)
+	}
+
+	// initialize serial comm
 	toSerialCh <- "hello"
 	log.Print("waiting on ack: ")
 	ackHello := <-fromSerialCh
@@ -74,29 +94,58 @@ func main() {
 	}
 	log.Println("done!")
 
-	// read def file
-	cfg := Cfg{}
-	cfgf, e := ioutil.ReadFile(".local/clickerd.conf")
-	if e != nil {
-		log.Fatalf("failed to read configuration: %v", e)
-	}
-
-	if e := yaml.Unmarshal([]byte(cfgf), &cfg); e != nil {
-		log.Fatalf("failed to unmarshal configuration: %v", e)
-	}
-
 	// send list of items
 	for i, it := range cfg.Items {
-		if len(it.Uri) == 0 {
-			it.Uri = cfg.Uri
-		}
-		println(i, it.Title, it.Uri, it.Body)
+		println(i, it.Title)
 		toSerialCh <- fmt.Sprintf("%v. %v", i, it.Title)
 	}
+
+	var last *Item = new(Item)
 
 	// listen for selections
 	for {
 		e := <-fromSerialCh
-		print(e)
+		switch {
+
+		// X=id
+		case strings.HasPrefix(e, "X="):
+			id, _ := strconv.ParseInt(strings.TrimPrefix(e, "X="), 10, 8)
+
+			log.Printf("selected index %v", id)
+			item := cfg.Items[id]
+			binary, _ := exec.LookPath(cfg.Command)
+
+			for _, m := range item.Modules {
+				args := []string{m.Id, m.Model}
+				for i := range last.Modules {
+					// look for the previous model and send it over
+					if last.Modules[i].Id == m.Id {
+						args = append(args, last.Modules[i].Model)
+						break
+					}
+				}
+
+				print(cfg.Command)
+				print(" ")
+				for i := range args {
+					print(args[i])
+					print(" ")
+				}
+				println()
+
+				cmd := exec.Command(binary, args...)
+				var out bytes.Buffer
+				var stderr bytes.Buffer
+				cmd.Stdout = &out
+				cmd.Stderr = &stderr
+
+				e := cmd.Run()
+				if e != nil {
+					log.Printf("failed to run command: %v", e)
+				}
+			}
+
+			last = &item
+		}
 	}
 }
