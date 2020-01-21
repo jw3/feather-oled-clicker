6baaecaeea82c5d2c8486b2182fad74645382cec
John Wass <wassj@ctc.com>
2020-01-21 08:10:40 -0500

    sample script

---
+++ clickerd/bin/model.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+readonly usage="usage: $0 <device-id> <model-json>"
+readonly device="${1?${usage}}"
+readonly model="${2?${usage}}"
+
+particle call $device addNodes "$model"
+particle call $device align
