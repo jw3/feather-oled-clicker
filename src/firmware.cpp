53f5449e23aa83a01d3b9abed9bf29ebea770a57
John Wass <wassj@ctc.com>
2020-01-21 13:04:41 -0500

    add double click icon

--- src/firmware.cpp
+++ src/firmware.cpp
@@ -7,6 +7,9 @@
 
 SYSTEM_MODE(MANUAL)
 
+const uint8_t ClickedIcon[] = {0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x3f, 0xf8, 0x80, 0x60, 0x01, 0x80, 0x60, 0x03, 0x00, 0x60, 0x06, 0x00, 0x60, 0x0c, 0x00, 0x62, 0x18, 0x00, 0x63, 0x31, 0x80,
+                               0x61, 0xe1, 0x80, 0x60, 0xc1, 0x80, 0x60, 0x01, 0x80, 0x60, 0x01, 0x80, 0x60, 0x01, 0x80, 0x60, 0x01, 0x80, 0x3f, 0xff, 0x00, 0x1f, 0xfe, 0x00, 0x00, 0x00, 0x00};
+
 const uint16_t DataPin = D6;
 const uint16_t ClockPin = D8;
 const uint16_t SwitchPin = D7;
@@ -46,6 +49,7 @@ bool ackd = false;
 bool initd = false;
 long last_click = 0;
 long last_dblclick = 0;
+long last_dblclick_idx = -1;
 void loop() {
    while(Serial.available()) {
       auto c = Serial.read();
@@ -98,6 +102,8 @@ void loop() {
          tft.clearDisplay();
          tft.setCursor(0, 0);
          tft.printf("%2i. %s", menuIdx + 1, clicklist.at(menuIdx).c_str());
+         if(menuIdx == last_dblclick_idx)
+            tft.drawBitmap(110, 15, ClickedIcon, 18, 18, 1);
          tft.display();
       }
 
@@ -106,10 +112,9 @@ void loop() {
       if(clicker.rose()) {
          if(t - last_click < 750 && t - last_dblclick > 2000) {
             // double click
-            auto idx = encoderIdx / 2 % clicklist.size();
-            if(idx < 0) idx += clicklist.size();
-            Serial.printlnf("X=%i", idx);
-            tft.print("!!");
+            Serial.printlnf("X=%i", menuIdx);
+            last_dblclick_idx = menuIdx;
+            tft.drawBitmap(110, 15, ClickedIcon, 18, 18, 1);
             tft.display();
 
             last_dblclick = t;
